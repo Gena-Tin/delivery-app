365ad8e0361c0da2b7054d401db13ee891d0ca0f
GenaTin <111846926+Gena-Tin@users.noreply.github.com>
2026-07-30 21:52:22 +0300

    configure CORS and route handling for Express backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://delivery-app-gt.netlify.app", "http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 app.use(express.json());
 
 app.use("/api", shopRoutes); // Теперь все роуты доступны по путям /api/goods, /api/orders и т.д.
@@ -35,9 +41,13 @@ app.get("/api/health", async (req: Request, res: Response) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`[server]: Server is running at http://localhost:${PORT}`);
-});
+// чтобы на Vercel не плодились лишние логи слушателя портов оборачиваем в IF
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`[server]: Server is running at http://localhost:${PORT}`);
+  });
+}
+
 // Костыль для удержания Event Loop, если лоадер завершает процесс
 setInterval(() => {}, 1 << 30);
 
